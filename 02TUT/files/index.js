644853194dfdc92086bb93f21f15d1a873478162
unknown <shylesh1640@gmail.com>
2026-01-17 22:20:29 +0530

    done

---
+++ 02TUT/files/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+fs.readFile(path.join(__dirname,'files','starter.txt'),'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data.toString());
+});
+
+console.log('Hello...')
+
+process.on('uncaughtException', err => {
+    console.error(`There was an uncaught error: ${err}`);
+    process.exit(1);
+})
