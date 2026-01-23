65031bc041bc9aa88aee469b53ba30ba3b7e75fe
unknown <shylesh1640@gmail.com>
2026-01-23 06:59:28 +0530

    Update server.js

--- 06TUT/server.js
+++ 06TUT/server.js
@@ -13,7 +13,36 @@ app.get('/index.html', (req, res) => {
 });
 
 app.get('/about',(req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new_page.html'));
+    res.redirect(301,'/new-page.html');
+});
+
+//route handlers
+app.get('/hello', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next();
+}, (req, res) => {
+    res.send("Hello world!")
+});
+
+//changing route managers
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}   
+const three = (req, res) => {
+    console.log('three');
+    res.send('Finished!');
+}
+
+app.get('/chain', [one, two, three]);
+
+// 404 catch-all handler - must be last
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
