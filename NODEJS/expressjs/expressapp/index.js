937473b1033600c8da9de91aeae5e488e50ea6f5
Shylesh S <shylesh1640@gmail.com>
2026-02-25 23:21:17 +0530

    Add middleware for logging HTTP requests

--- NODEJS/expressjs/expressapp/index.js
+++ NODEJS/expressjs/expressapp/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 app.get("/",(req,res) => {
     res.status(200).send("Welcome to Express.js!");
 })
